21c2ee5ea319db2596f887429287a512b2c70f94
hokageCodes <hashsenju94@gmail.com>
2023-01-12 19:24:41 +0100

    New party deal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 
 import './App.css';
-import Hero from './components/Hero';
-import HowItWorks from './components/HowItWorks';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
-      <HowItWorks />
+      <h1>Hello, World</h1>
     </div>
   );
 }
